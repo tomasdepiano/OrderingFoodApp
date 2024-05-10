a7b7bb2b6586f5ff08df3a232dead3b63f70f502
Tomas De Piano <tomasdepiano1@gmail.com>
2024-05-10 11:58:17 -0600

    Used flatList to display all the dummy data on the menu screen, which makes screen scrollable; added some css properties to show multiple pizzas on the screen at once as well

--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -16,6 +16,7 @@ const ProductListItem = ({ product }: ProductListItemProps) => {
       <Image
         source={{ uri: product.image || defaultPizzaImage }}
         style={styles.image}
+        resizeMode="contain"
       />
       <Text style={styles.title}>{product.name}</Text>
       <Text style={styles.price}>${product.price}</Text>
@@ -28,6 +29,8 @@ const styles = StyleSheet.create({
     backgroundColor: "white",
     padding: 10,
     borderRadius: 20,
+    flex: 1,
+    maxWidth: "50%",
   },
   image: { width: "100%", aspectRatio: 1 },
   title: { fontSize: 18, fontWeight: "600", marginVertical: 10 },
