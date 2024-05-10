a963d8b9523836b6165eab26456e764da787315d
Tomas De Piano <tomasdepiano1@gmail.com>
2024-05-09 20:03:11 -0600

    Completed the UI for just one pizza and works just fine

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,35 +1,31 @@
-import { StyleSheet } from "react-native";
-
+import { StyleSheet, Image } from "react-native";
+import Colors from "@/src/constants/Colors";
+import products from "@/assets/data/products";
 import EditScreenInfo from "@/src/components/EditScreenInfo";
 import { Text, View } from "@/src/components/Themed";
+
+const product = products[1];
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Hello World</Text>
-      <View
-        style={styles.separator}
-        lightColor="#eee"
-        darkColor="rgba(255,255,255,0.1)"
-      />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <Image source={{ uri: product.image }} style={styles.image} />
+      <Text style={styles.title}>{product.name}</Text>
+      <Text style={styles.price}>${product.price}</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: "white",
+    padding: 10,
+    borderRadius: 20,
   },
-  title: {
-    fontSize: 20,
+  image: { width: "100%", aspectRatio: 1 },
+  title: { fontSize: 18, fontWeight: "600", marginVertical: 10 },
+  price: {
+    color: Colors.light.tint,
     fontWeight: "bold",
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: "80%",
   },
 });
