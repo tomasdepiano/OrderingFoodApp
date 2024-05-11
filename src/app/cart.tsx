75579697c574347e6f74dd002d00e5f64f966035
Tomas De Piano <tomasdepiano1@gmail.com>
2024-05-11 16:58:23 -0600

    added more logic so the cart will get updated and dipslay the price of the items using reduce; also if user adds more of the same item in cart it will do the math and display the total

--- src/app/cart.tsx
+++ src/app/cart.tsx
@@ -2,16 +2,21 @@ import { View, Text, Platform, FlatList } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { useCart } from "../providers/CartProvider";
 import CartListItem from "../components/CartListItem";
+import Button from "../components/button";
 
 const CartScreen = () => {
-  const { items } = useCart();
+  const { items, total } = useCart();
   return (
-    <View>
+    <View style={{ padding: 10 }}>
       <FlatList
-        contentContainerStyle={{ padding: 10, gap: 10 }}
+        contentContainerStyle={{ gap: 10 }}
         data={items}
         renderItem={({ item }) => <CartListItem cartItem={item} />}
       />
+      <Text style={{ marginTop: 20, fontSize: 20, fontWeight: "500" }}>
+        Total: ${total}
+      </Text>
+      <Button text="Checkout" />
       <StatusBar style={Platform.OS === "ios" ? "light" : "auto"} />
     </View>
   );
--- src/providers/CartProvider.tsx
+++ src/providers/CartProvider.tsx
@@ -6,12 +6,14 @@ type CartType = {
   items: CartItem[];
   addItem: (product: Product, size: CartItem["size"]) => void;
   updateQuantity: (itemId: string, amount: -1 | 1) => void;
+  total: number;
 };
 
 const CartContext = createContext<CartType>({
   items: [],
   addItem: () => {},
   updateQuantity: () => {},
+  total: 0,
 });
 
 const CartProvider = ({ children }: PropsWithChildren) => {
@@ -50,12 +52,13 @@ const CartProvider = ({ children }: PropsWithChildren) => {
     );
   };
 
-  //update Quantity
-
-  //   console.log(items);
+  const total = items.reduce(
+    (sum, item) => (sum += item.product.price * item.quantity),
+    0
+  );
 
   return (
-    <CartContext.Provider value={{ items, addItem, updateQuantity }}>
+    <CartContext.Provider value={{ items, addItem, updateQuantity, total }}>
       {children}
     </CartContext.Provider>
   );
