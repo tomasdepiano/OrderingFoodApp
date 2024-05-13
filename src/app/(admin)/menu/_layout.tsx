f2ec7ea0a9502bff4ed64d965c516c4b1bcab047
Tomas De Piano <tomasdepiano1@gmail.com>
2024-05-12 18:09:09 -0600

    Created the screen to create a product; only shows on admin side but gives the admin the ability to select a photo from their phone library using expo image picker; set up some validation as well in case they put the wrong input in the input boxes

--- src/app/(admin)/menu/_layout.tsx
+++ src/app/(admin)/menu/_layout.tsx
@@ -28,7 +28,7 @@ export default function MenuStack() {
         options={{
           title: "Menu",
           headerRight: () => (
-            <Link href="/" asChild>
+            <Link href="/(admin)/menu/create" asChild>
               <Pressable>
                 {({ pressed }) => (
                   <FontAwesome
