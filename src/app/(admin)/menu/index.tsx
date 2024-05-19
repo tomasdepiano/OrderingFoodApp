24e37c20e40a93c93b3ee99fc5deaa457aad4666
Tomas De Piano <tomasdepiano1@gmail.com>
2024-05-19 17:19:51 -0600

    downloaded react query to be able to fetch data from supabase databse; got it to work and on both admin and user side; created a custom hooks to fetch the data from DB and implemented it on the screens it needs to go to

--- src/app/(admin)/menu/index.tsx
+++ src/app/(admin)/menu/index.tsx
@@ -1,9 +1,18 @@
-import { View } from "@/src/components/Themed";
-import { FlatList } from "react-native";
-import products from "@/assets/data/products";
+import { FlatList, Text } from "react-native";
 import ProductListItem from "@/src/components/ProductListItem";
+import { useProductList } from "@/src/api/products";
+import { ActivityIndicator } from "react-native";
 
 export default function MenuScreen() {
+  const { data: products, error, isLoading } = useProductList();
+
+  if (isLoading) {
+    return <ActivityIndicator />;
+  }
+
+  if (error) {
+    return <Text>Failed to fetch products</Text>;
+  }
   return (
     <FlatList
       data={products}
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -12,6 +12,7 @@ import "react-native-reanimated";
 import CartProvider from "../providers/CartProvider";
 import AuthProvider from "../providers/AuthProvider";
 import { useColorScheme } from "@/src/components/useColorScheme";
+import QueryProvider from "../providers/QueryProvider";
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -56,14 +57,16 @@ function RootLayoutNav() {
   return (
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
       <AuthProvider>
-        <CartProvider>
-          <Stack>
-            <Stack.Screen name="(admin)" options={{ headerShown: false }} />
-            <Stack.Screen name="(user)" options={{ headerShown: false }} />
-            <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-            <Stack.Screen name="cart" options={{ presentation: "modal" }} />
-          </Stack>
-        </CartProvider>
+        <QueryProvider>
+          <CartProvider>
+            <Stack>
+              <Stack.Screen name="(admin)" options={{ headerShown: false }} />
+              <Stack.Screen name="(user)" options={{ headerShown: false }} />
+              <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+              <Stack.Screen name="cart" options={{ presentation: "modal" }} />
+            </Stack>
+          </CartProvider>
+        </QueryProvider>
       </AuthProvider>
     </ThemeProvider>
   );
